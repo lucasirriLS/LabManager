f37b7abc0fbc89082e66041c87662b9410b6eaa7
Luca <Luca@MTD_DLL_003>
2014-06-06 14:33:05 +0200

    foglio di stile

--- src/it/rn14/managerlab/lab/LabDettaglio.java
+++ src/it/rn14/managerlab/lab/LabDettaglio.java
@@ -9,6 +9,7 @@ import it.rn14.managerlab.proxyobj.Rover;
 public class LabDettaglio {
 	
 	private Lab lab;
+	private Integer slot;
 
 	public Lab getLab() {
 		
@@ -16,26 +17,36 @@ public class LabDettaglio {
 		lab = new Lab();
 		lab.setTitle("Lab test");
 		
-		popolateLabForTest(lab);
+		popolateLabForTest(lab,1);
+		popolateLabForTest(lab,2);
+		popolateLabForTest(lab,3);
 		return lab;
 	}
 
-	private void popolateLabForTest(Lab lab) {
+	private void popolateLabForTest(Lab lab, int slot) {
 		ArrayList<Rover> partecipanti = new ArrayList<Rover>();
 		for (int i = 0; i < 35; i++) {
 			Rover rover = new Rover();
 			rover.setAge(i);
-			rover.setName("name" + i);
-			rover.setSurname("surname" + i);
+			rover.setName("name " + i + " Slot " + slot);
+			rover.setSurname("surname " + i);
 			partecipanti.add(rover);
 		}
-		lab.setPartecipant1(partecipanti);
-		lab.setPartecipant2(partecipanti);
-		lab.setPartecipant3(partecipanti);
+		if(slot==1)	lab.setPartecipant1(partecipanti);
+		if(slot==2)	lab.setPartecipant2(partecipanti);
+		if(slot==3)	lab.setPartecipant3(partecipanti);
 	}
 
 	public void setLab(Lab lab) {
 		this.lab = lab;
+	}
+
+	public Integer getSlot() {
+		return slot;
+	}
+
+	public void setSlot(Integer slot) {
+		this.slot = slot;
 	}
 	
 	
