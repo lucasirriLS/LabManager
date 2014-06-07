b80ae4be95b28291c1ae90b6acea58b3eeae3a36
Luca <Luca@Luca-HP>
2014-06-07 11:51:20 +0200

    gestione e aggiornamento lista partecipanti

--- src/it/rn14/managerlab/lab/LabDettaglio.java
+++ src/it/rn14/managerlab/lab/LabDettaglio.java
@@ -1,15 +1,17 @@
 package it.rn14.managerlab.lab;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import it.rn14.managerlab.proxyobj.Group;
 import it.rn14.managerlab.proxyobj.Lab;
 import it.rn14.managerlab.proxyobj.Rover;
+
+import java.util.ArrayList;
+
+import javax.faces.event.ValueChangeEvent;
 
 public class LabDettaglio {
 	
 	private Lab lab;
-	private Integer slot;
+	private Integer slot = 1;
 
 	public Lab getLab() {
 		
@@ -22,19 +24,46 @@ public class LabDettaglio {
 		popolateLabForTest(lab,3);
 		return lab;
 	}
+	
+	public void cambioslot(ValueChangeEvent e){
+		System.out.println("LabDettaglio.valueChangeMethod()" + e.getNewValue().toString());
+	}
 
 	private void popolateLabForTest(Lab lab, int slot) {
 		ArrayList<Rover> partecipanti = new ArrayList<Rover>();
-		for (int i = 0; i < 35; i++) {
+		int limit = 35 - slot;
+		for (int i = 0; i < limit; i++) {
 			Rover rover = new Rover();
 			rover.setAge(i);
 			rover.setName("name " + i + " Slot " + slot);
 			rover.setSurname("surname " + i);
+			rover.setGroup(new Group("gruppo X", 123));
 			partecipanti.add(rover);
 		}
 		if(slot==1)	lab.setPartecipant1(partecipanti);
 		if(slot==2)	lab.setPartecipant2(partecipanti);
 		if(slot==3)	lab.setPartecipant3(partecipanti);
+	}
+	
+	public Integer getNumeroPartecipanti(){
+		ArrayList<Rover> rovers = getPartecipanti();
+		if(rovers!=null) return rovers.size();
+		else return 0;
+	}
+	
+	public ArrayList<Rover> getPartecipanti(){
+		if(slot==null) slot=1;
+		if(lab==null) lab=getLab();
+		
+		if(slot==1)	
+			return (ArrayList<Rover>) lab.getPartecipant1();	
+		if(slot==2)	
+			return (ArrayList<Rover>) lab.getPartecipant2();
+		if(slot==3)	
+			return (ArrayList<Rover>) lab.getPartecipant3();
+		
+		return null;
+		
 	}
 
 	public void setLab(Lab lab) {
--- src/it/rn14/managerlab/proxyobj/Group.java
+++ src/it/rn14/managerlab/proxyobj/Group.java
@@ -6,5 +6,35 @@ public class Group {
 	private long code;
 	private int subcamp;
 	private int twinning;
+	
+	public Group(String name, long code) {
+		super();
+		this.name = name;
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getCode() {
+		return code;
+	}
+	public void setCode(long code) {
+		this.code = code;
+	}
+	public int getSubcamp() {
+		return subcamp;
+	}
+	public void setSubcamp(int subcamp) {
+		this.subcamp = subcamp;
+	}
+	public int getTwinning() {
+		return twinning;
+	}
+	public void setTwinning(int twinning) {
+		this.twinning = twinning;
+	}
 
 }
